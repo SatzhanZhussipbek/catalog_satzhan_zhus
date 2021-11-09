cbd8aba6e770935834d6106c2108ea6c7d630eef
Satzhan Zhussipbek <sadz_kz@mail.ru>
2021-11-09 13:48:47 +0600

    Changed jpa_lessons_satzhan

--- src/main/java/satzhanzhus/jpa_lessons_satzhan/Create.java
+++ src/main/java/satzhanzhus/jpa_lessons_satzhan/Create.java
@@ -9,71 +9,95 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-// https://habr.com/ru/post/265061/ - шпаргалка по Java
 
 public class Create {
     public static void main(String[] args) {
-        // Создать интерфейс для товара, для того, чтобы он сохранился в базе данных
-        // Комплектующие [1]
-        // Аудиотехника [2]
-        // Мебель [3]
-        // Выберите категорию (номер): ___
-        // Введите название: ___
-        // Введите цену: ___
+
+        System.out.println("Создание категории[1]\nИзменение товара[2]\nУдаление категории[3]\nВыберите действие: ");
+        Scanner scannerApp = new Scanner(System.in);
+        String answerIn = scannerApp.nextLine();
+        switch (answerIn) {
+            case "1":
+                create();
+                break;
+            case "2":
+                change();
+                break;
+            case "3":
+                delete();
+                break;
+        }
+
+    }
+    private static void create() {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
         EntityManager manager = factory.createEntityManager();
 
-        Scanner scanner = new Scanner(System.in); // класс Scanner для считывания с консоли
-        // .nextLine - метод, который считывает одну полную строку с консоли
+        Scanner scanner = new Scanner(System.in);
 
         Product product = new Product();
-        Product product2 = new Product();
 
-        // Создание запроса в JPQL
-        //TypedQuery<Category> query = manager.createQuery("select c from Category c", Category.class);
-        //List<Category> categories = query.getResultList(); // вытаскивает из запроса результат в виде списка
-        TypedQuery<Product> query2 = manager.createQuery("select p from Product p", Product.class);
-        List<Product> products = query2.getResultList();
-        //TypedQuery<Feature> query3 = manager.createQuery("select f from Feature f", Feature.class);
-        //List<Feature> features = query3.getResultList();
-        /*for (Category category : categories) {
+        TypedQuery<Category> query2 = manager.createQuery("Select c from Category c", Category.class);
+        List<Category> categories = query2.getResultList();
+        for (Category category: categories) {
             System.out.println(category.getName() + "[" + category.getId() + "]");
-        }*/
+        }
+        //--- Создание категории ---
+        // Введите название категории: ___
+        // введите характеристики категории через запятую: ___
+
+        Category category = new Category();
+        List<Feature> features = new ArrayList<>();
         try {
             manager.getTransaction().begin();
-            /*System.out.println("Выберите категорию (номер): ");
-            String categoryNumIn = scanner.nextLine();
-            product.setCategory(manager.find(Category.class, Long.parseLong(categoryNumIn) ));
+            System.out.println("Введите название категории: ");
+            String categoryNameIn = scanner.nextLine();
+            TypedQuery<Long> query4 = manager.createQuery("Select count(c.id) from Category c where c.name = ?1 ", Long.class);
+            query4.setParameter(1, categoryNameIn);
+            List<Long> categories3 = query4.getResultList();
+            // while (categories3.get(0) != 0)
+            while (!categories3.contains(0L))
+            {
+                System.out.println("Такая категория уже существует!");
+                System.out.println("Введите название категории: ");
+                categoryNameIn = scanner.nextLine();
+                query4.setParameter(1, categoryNameIn);
+                categories3 = query4.getResultList();
+            }
+            category.setName(categoryNameIn);
+            System.out.println("Введите характеристики категории через запятую: ");
+            String categoryFeatures = scanner.nextLine();
+            String[] singleFeature = categoryFeatures.split(", ");
+            manager.persist(category);
+            for (String a : singleFeature) {
+                Feature feature = new Feature();
+                feature.setCategory(category);
+                feature.setFeatures(a);
+                features.add(feature);
+                category.setFeatures(features);
+                manager.persist(feature);
+            }
 
-            System.out.println("Введите название: ");
-            String productNameIn = scanner.nextLine();
-            product.setName(productNameIn);
+            manager.getTransaction().commit();
+        }
+        catch (Exception e ) {
+            manager.getTransaction().rollback();
+            e.printStackTrace();
+        }
+    }
+    private static void change() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
+        EntityManager manager = factory.createEntityManager();
+        Scanner scanner = new Scanner(System.in);
+        Product product = new Product();
+        // Создание запроса в JPQL
+        TypedQuery<Product> query2 = manager.createQuery("select p from Product p", Product.class);
+        List<Product> products = query2.getResultList();
 
-            System.out.println("Введите цену");
-            String productPriceIn = scanner.nextLine();
-            product.setPrice(Long.parseLong(productPriceIn));*/
-
-            //System.out.println("Выберите характеристику");
-            // Вывести характеристики выбранной категории товара
-            // Диагональ: ___
-            // Матрица: ___
-            // Записать данные характеристик в features_values
-            //manager.persist(product); // нужно писать до manager.getTransaction().commit, но после
-            // занесения всех данных
-            String featureVal1 = "";
-
-            /*for (Feature feature : product.getCategory().getFeatures()) {
-                System.out.println(feature.getFeatures() + ":");
-                featureVal1 = scanner.nextLine();
-
-                FeatureValue featureValue = new FeatureValue();
-                featureValue.setFeatureValue(featureVal1);
-                featureValue.setFeature(feature);
-                featureValue.setProduct(product);
-                manager.persist(featureValue);
-            }*/
+        try {
             // Еще одно задание: реализовать обновление товара, т.е.
             // Запрос id товара, потом ввести новое название и новую цену, затем необходимо записать запрошенные данные в базу.
             // Изменить значения характеристик товара
@@ -83,18 +107,18 @@ public class Create {
             System.out.println("Выберите номер товара: ");
             String productIdIn = scanner.nextLine();
             product = manager.find(Product.class, Long.parseLong(productIdIn));
-            // если ничего не вводится, то должно остаться предыдущее значения при изменении
+            // если ничего не вводится, то должно остаться предыдущее значение при изменении
             // товара и его характеристик
             System.out.println("Введите новое название: ");
-            String productNameIn = scanner.nextLine();
-            if (!productNameIn.isEmpty()) {
-             product.setName(productNameIn);
+            String productNameInNew = scanner.nextLine();
+            if (!productNameInNew.isEmpty()) {
+                product.setName(productNameInNew);
             }
 
             System.out.println("Введите новую цену: ");
-            String productPriceIn = scanner.nextLine();
-            if (!productPriceIn.isEmpty()) {
-             product.setPrice(Long.parseLong(productPriceIn));
+            String productPriceInNew = scanner.nextLine();
+            if (!productPriceInNew.isEmpty()) {
+                product.setPrice(Long.parseLong(productPriceInNew));
             }
             manager.persist(product);
             // Здесь начинается изменение значения характеристик товара
@@ -117,16 +141,33 @@ public class Create {
                     }
                     manager.persist(fVal);
                 }
-
-                /*System.out.println("Введите новое название характеристики: ");
-                String featureNameIn = scanner.nextLine();
-                featureValue2.setFeature(feature);
-                featureValue2.setFeatureValue(featureVal1);
-                featureValue2.setProduct(product);
-                featureValue2.setFeature(feature);
-                manager.persist(featureValue2);*/
             }
 
+            manager.getTransaction().commit();
+        }
+        catch (Exception e) {
+            manager.getTransaction().rollback();
+            e.printStackTrace();
+        }
+    }
+
+    private static void delete() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
+        EntityManager manager = factory.createEntityManager();
+        Scanner scanner = new Scanner(System.in);
+        TypedQuery<Category> query = manager.createQuery("Select c from Category c", Category.class);
+        List<Category> categories = query.getResultList();
+        for (Category category: categories) {
+            System.out.println(category.getName() + "[" + category.getId() + "]");
+        }
+        System.out.println("Выберите категорию, которую хотите удалить: ");
+        String categoryId = scanner.nextLine();
+       // Удаление объекта локально от EntityManager, превращая category4 из сущности
+        // в обычный объект. Удаление из базы происходит, если удаление стоит в рамках транзакции.
+        Category category4 = manager.find(Category.class, Long.parseLong(categoryId));
+        try {
+            manager.getTransaction().begin();
+            manager.remove(category4);
             manager.getTransaction().commit();
         }
         catch (Exception e) {
